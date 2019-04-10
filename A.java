5bb536a8dc129790adced1e4a1e0639cbbee151a
michellenc <49461622+michellenc@users.noreply.github.com>
2019-04-09 22:32:25 -0300

    Add files via upload

---
+++ A.java
@@ -0,0 +1,34 @@
+
+public class A {
+    private int a1;
+    private float a2;
+    public java.lang.String nome1 = "MA1";
+    public java.lang.String nome2 = "MA2";
+    
+    public int getAtributo1()  {
+      return a1;
+    }
+   
+    public float getAtributo2() 
+    {
+      return a2;
+    }
+    
+    public void setAtributo1(int a1) 
+    {
+      this.a1 = a1;
+    }
+    
+    public void setAtributo2(float a2) 
+    {
+      this.a2 = a2;
+    }
+    
+    public java.lang.String MA1() {
+        return nome1;
+    }
+    
+    public java.lang.String MA2(){
+      return nome2;
+    }
+}
---
+++ B.java
@@ -0,0 +1,33 @@
+ public class B {
+    private int b1;
+    private float b2;
+    public java.lang.String nome1 = "MB1";
+    public java.lang.String nome2 = "MB2";
+
+      public int getAtributo1()  {
+      return b1;
+    }
+   
+    public float getAtributo2() 
+    {
+      return b2;
+    }
+    
+    public void setAtributo1(int b1) 
+    {
+      this.b1 = b1;
+    }
+    
+    public void setAtributo2(float b2) 
+    {
+      this.b2 = b2;
+    }
+    
+    public java.lang.String MB1() {
+     return nome1;
+    }
+    
+    public java.lang.String MB2(){
+     return nome2;
+    }
+}
