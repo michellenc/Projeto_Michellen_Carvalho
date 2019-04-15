c44120342774e6dfb82dc6eceaab00e450138a9c
michellenc <49461622+michellenc@users.noreply.github.com>
2019-04-15 11:03:55 -0300

    Add files via upload

---
+++ D.java
@@ -0,0 +1,19 @@
+ public class D
+ {
+    private int d1;
+    private int d2;
+    
+    public D()
+    {
+     d1 = 20;
+	 d2 = 10;
+    }
+
+    public int MD1(int d1, int d2) {
+        return d1 + d2;
+    }
+    
+    public int MD2(int d1, int d2){
+      return d1 - d2;
+    }
+} 
