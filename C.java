d094a512647440f8f78834aa30fe0624e5848958
michellenc <49461622+michellenc@users.noreply.github.com>
2019-04-15 10:11:58 -0300

    Add files via upload

---
+++ C.java
@@ -0,0 +1,21 @@
+ 
+ public class C
+ {
+    private String c1;
+    private int c2;
+    
+    public C()
+    {
+     c2 = 0;
+    }
+
+    public String MC1( String c1) {
+        c1 = "MC1";
+        return c1;
+    }
+    
+    public String MC2( String c2){
+      c2 = "MC2";
+      return c2;
+    }
+}
